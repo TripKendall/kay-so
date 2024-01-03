c3761277fa738e97292e01c36f43dff3945ceb97
TripKendall <tripkendall@gmail.com>
2024-01-03 11:23:38 -0500

    getting close...

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,7 +3,7 @@
     <div class="container w-75">
       <div class="hero-section-content text-center">
         <img :src="pageData.image" :alt="pageData.subtitle" class="img-fluid">
-        <h2 class="title text-uppercase">
+        <h2 class="title text-uppercase mt-3">
           Kay<span class="primary-text">-So</span>
         </h2>
         <h3>
@@ -12,13 +12,27 @@
         <p class="text">
           {{ pageData.description }}
         </p>
-        <audio controls>
-              <source src="/audio/kay-so-one.mp3" type="audio/mpeg">
-              Your browser does not support the audio tag.
-            </audio>
-        <p>
+
+        <div class="kay-so-podcast">
+          <div class="kay-so-podcast-episode">
+        <h5>Episode One - Your listening pleasures</h5>
+          <audio controls>
+            <source src="/audio/kay-so-one.mp3" type="audio/mpeg">
+            Your browser does not support the audio tag.
+          </audio>
+        </div>
+
+        <div class="kay-so-podcast-episode">
+        <h5>Episode Two - Nevermore</h5>
+          <audio controls>
+            <source src="/audio/kay-so-two-nevermore.mp3" type="audio/mpeg">
+            Your browser does not support the audio tag.
+          </audio>
+        </div>
+        </div>
+        <!-- <p>
           <a href="#what">More Info...</a>
-        </p>
+        </p> -->
       </div>
     </div>
   </section>
@@ -27,16 +41,11 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-// import { defineProps } from 'vue'
-//   const props = defineProps({
-//     msg: String
-//   })
-
 const pageData = ref(
   {
     title: 'Kay-So',
     subtitle: 'For your listening pleasures...',
-    description: 'The Kay-So Podcast is a collection of audio flotsam and jetsam from the interwebs filtered through the mind of Kay-So.',
+    description: 'The Kay-So Podcast is a collection of audio flotsam and jetsam from the interwebs filtered through the mind of Kay-So.  If you are interested in listening differently this podcast may be for you. I like to think of it as a podcast palate cleanse.',
     image: '/images/kay-so-280.webp',
     imageAlt: 'Kay-So',
   }
@@ -45,26 +54,38 @@ const pageData = ref(
 </script>
 
 <style scoped>
-
 .hero {
-  background-image: linear-gradient(
-  45deg,
-  hsl(0deg 0% 5%) 0%,
-  hsl(344deg 0% 4%) 10%,
-  hsl(344deg 0% 3%) 20%,
-  hsl(344deg 0% 2%) 30%,
-  hsl(344deg 0% 1%) 40%,
-  hsl(0deg 0% 0%) 50%,
-  hsl(344deg 0% 1%) 60%,
-  hsl(344deg 0% 3%) 70%,
-  hsl(344deg 0% 4%) 80%,
-  hsl(344deg 0% 5%) 90%,
-  hsl(0deg 0% 6%) 100%
-);
+  background-image: linear-gradient(45deg,
+    hsl(0deg 0% 5%) 0%,
+    hsl(344deg 0% 4%) 10%,
+    hsl(344deg 0% 3%) 20%,
+    hsl(344deg 0% 2%) 30%,
+    hsl(344deg 0% 1%) 40%,
+    hsl(0deg 0% 0%) 50%,
+    hsl(344deg 0% 1%) 60%,
+    hsl(344deg 0% 3%) 70%,
+    hsl(344deg 0% 4%) 80%,
+    hsl(344deg 0% 5%) 90%,
+    hsl(0deg 0% 6%) 100%);
 }
 
 .download-button {
   margin: .3rem 0 3rem 0;
+}
+
+.kay-so-podcast{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.kay-so-podcast-episode {
+  margin: 1.5rem 0;
+  padding: 1rem;
+  background-color: rgba(12, 12, 12, .3);
+  min-width: 640px;
+  border-radius: .33rem;
+  border: 5px solid rgba(0, 0, 0, .3);
 }
 
 audio::-webkit-media-controls-enclosure {
@@ -113,5 +134,4 @@ audio::-webkit-media-controls-volume-slider {
   padding-left: 8px;
   padding-right: 8px;
 }
-
 </style>
